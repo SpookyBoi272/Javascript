2ebc8826a94e5191f73f0d76bc85219233dc0208
Spooky Boi <kattelniranjan@gmail.com>
2023-04-29 18:50:33 +0545

    api trial

---
+++ index.js
@@ -0,0 +1,18 @@
+const wordForm = document.getElementById('wordForm');
+wordForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // prevent form from submitting and refreshing the page
+  
+  const wordInput = document.getElementById('wordInput').value;
+  const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput}`;
+  
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // handle the API response data here
+      console.log(data);
+    })
+    .catch(error => {
+      // handle any errors here
+      console.error(error);
+    });
+});
